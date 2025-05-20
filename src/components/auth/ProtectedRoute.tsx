db09473addc89796feb66dbdcaf345b728800a0c
ahmed yasser <ahmedya3ser8@gmail.com>
2025-05-20 18:58:56 +0300

    enhance protected and public routes

--- src/components/auth/ProtectedRoute.tsx
+++ src/components/auth/ProtectedRoute.tsx
@@ -1,17 +1,26 @@
 "use client"
 import { useAppSelector } from "@store/hooks"
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { LuLoaderCircle } from "react-icons/lu";
 
-export default function ProtectedRoute({ children }: {children: React.ReactNode}) {
+export default function ProtectedRoute({ children, redirectTo = "/auth/login" }: {children: React.ReactNode, redirectTo?: string}) {
   const { token } = useAppSelector(state => state.auth);
   const router = useRouter();
+  const [checkingAuth, setCheckingAuth] = useState(true);
   useEffect(() => {
-    if (token == null) {
-      router.push('/auth/login');
+    setCheckingAuth(false);
+    if (!token) {
+      router.push(redirectTo);
     }
-  }, [token, router]);
-  if (token == null) return null;
+  }, [token, router, redirectTo]);
+  if (checkingAuth || !token) { 
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <LuLoaderCircle className="animate-spin size-10" />
+      </div>
+    )
+  }
   return (
     <>{children}</>
   )
--- src/components/auth/PublicRoute.tsx
+++ src/components/auth/PublicRoute.tsx
@@ -1,17 +1,27 @@
 "use client"
 import { useAppSelector } from "@store/hooks";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { LuLoaderCircle } from "react-icons/lu";
 
-export default function PublicRoute({ children }: {children: React.ReactNode}) {
+export default function PublicRoute({ children, redirectTo = "/" }: {children: React.ReactNode, redirectTo?: string}) {
   const { token } = useAppSelector(state => state.auth);
   const router = useRouter();
+  const [checkingAuth, setCheckingAuth] = useState(true);
+
   useEffect(() => {
+    setCheckingAuth(false);
     if (token) {
-      router.push('/');
+      router.push(redirectTo);
     }
-  }, [token, router]);
-  if (token) return null;
+  }, [token, router, redirectTo]);
+  if (checkingAuth || token) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <LuLoaderCircle className="animate-spin size-10" />
+      </div>
+    )
+  }
   return (
     <>{children}</>
   )
